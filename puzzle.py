3d494b8b8484a799dad44682a67534310b7cf4d9
Harsh <harshsharma102000@gmail.com>
2021-04-18 09:31:16 +0530

    New

--- puzzle.py
+++ puzzle.py
@@ -1,54 +1,146 @@
-class Puzzle:
-    def __init__(self, initialPositions, goalState):
-        super().__init__()
-        self.positions = initialPositions
-        self.zero = initialPositions.index(0)
-        self.goalState = goalState
+import copy
+from collections import deque
+from typing import List
 
-    def down(self):
-        if self.zero < 6:
-            self.positions[self.zero] = self.positions[self.zero+3]
-            self.positions[self.zero+3] = 0
-            self.zero = self.zero+3
-            return True
-        else:
-            return False
 
-    def up(self):
-        if self.zero > 2:
-            self.positions[self.zero] = self.positions[self.zero-3]
-            self.positions[self.zero-3] = 0
-            self.zero = self.zero-3
-            return True
-        else:
-            return False
+class State:
+    def __init__(self, board):
+        self.board = board
 
-    def left(self):
-        if self.zero % 3 != 0:
-            self.positions[self.zero] = self.positions[self.zero-1]
-            self.positions[self.zero-1] = 0
-            self.zero = self.zero-1
-            return True
-        else:
-            return False
+    def find_blank(self):
+        """return cooordinates of the blank tile"""
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j] == 0:
+                    return (i, j)
+        return (0, 0)
 
-    def right(self):
-        if self.zero % 3 != 2:
-            self.positions[self.zero] = self.positions[self.zero+1]
-            self.positions[self.zero+1] = 0
-            self.zero = self.zero+1
-            return True
-        else:
-            return False
+    def moves(self):
+        blank_i, blank_j = self.find_blank()
 
-    def isGoalState(self):
-        if self.goalState == self.positions:
-            print("Goal State is reached")
-            print("---------------------------")
-            return True
-        return False
+        # names of the moves
+        possible_moves = []
 
-    def displayState(self):
-        for i in range(0, 3):
-            print(self.positions[i*3 + 0],self.positions[i*3+1], self.positions[i*3+2])
-        print("---------------------------")
+        if blank_i == 0:
+            # down
+            possible_moves.append("D")
+        elif blank_i == 1:
+            # up
+            possible_moves.append("U")
+            # down
+            possible_moves.append("D")
+        elif blank_i == 2:
+            # up
+            possible_moves.append("U")
+
+        if blank_j == 0:
+            # right
+            possible_moves.append("R")
+        elif blank_j == 1:
+            # left
+            possible_moves.append("L")
+            # right
+            possible_moves.append("R")
+        elif blank_j == 2:
+            # left
+            possible_moves.append("L")
+
+        return possible_moves
+
+    def move_left(self, blank_i: int, blank_j: int):
+        self.board[blank_i][blank_j] = self.board[blank_i][blank_j - 1]
+        self.board[blank_i][blank_j - 1] = 0
+
+    def move_right(self, blank_i: int, blank_j: int):
+        self.board[blank_i][blank_j] = self.board[blank_i][blank_j + 1]
+        self.board[blank_i][blank_j + 1] = 0
+
+    def move_up(self, blank_i: int, blank_j: int):
+        self.board[blank_i][blank_j] = self.board[blank_i - 1][blank_j]
+        self.board[blank_i - 1][blank_j] = 0
+
+    def move_down(self, blank_i: int, blank_j: int):
+        self.board[blank_i][blank_j] = self.board[blank_i + 1][blank_j]
+        self.board[blank_i + 1][blank_j] = 0
+
+    def copy(self):
+        """return copy of the state"""
+        return State(board=copy.deepcopy(self.board))
+
+    def hash_str(self) -> str:
+        """return immutable/hashable value of state"""
+        hash_string = ""
+        for i in self.board:
+            for j in i:
+                hash_string += str(j) + "-"
+        return hash_string
+
+    def __repr__(self) -> str:
+        """return string representation (readable) of the board state"""
+        out_string = ""
+        for i in self.board:
+            for j in i:
+                out_string += str(j) + "\t"
+            out_string += "\n"
+        return out_string
+
+
+def compare_states(given_state: State, goal_state: State) -> int:
+    state_diffs = 0
+    for i in range(3):
+        for j in range(3):
+            if given_state.board[i][j] != goal_state.board[i][j]:
+                state_diffs += 1
+    return state_diffs
+
+
+def main():
+    init = State([[1, 2, 3], [8, 0, 4], [7, 6, 5]])
+    goal = State([[2, 8, 1], [0, 4, 3], [7, 6, 5]])
+
+    print("Initial State:")
+    print(init)
+    print("-" * 20)
+
+    stack = deque([init])
+    visited = {init.hash_str()}
+
+    while len(stack) > 0:
+
+        # BFS (Queue)
+        current_state = stack.popleft()
+
+        # DFS (Stack)
+
+        print("Computing:")
+        print(current_state)
+        if compare_states(goal, current_state) == 0:
+            print("Found:")
+            print(current_state)
+            break
+
+        for move in current_state.moves():
+            cp = current_state.copy()
+            blank_i, blank_j = cp.find_blank()
+            if move == "U":
+                cp.move_up(blank_i, blank_j)
+            elif move == "D":
+                cp.move_down(blank_i, blank_j)
+            elif move == "L":
+                cp.move_left(blank_i, blank_j)
+            elif move == "R":
+                cp.move_right(blank_i, blank_j)
+            else:
+                break
+
+            cp_hashable = cp.hash_str()
+            if cp_hashable not in visited:
+                visited.add(cp_hashable)
+                stack.append(cp)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,87 +1,117 @@
+# Getting clear output from Ipython
 from IPython.display import clear_output
-
-
-def display_board(board):
-    clear_output()  # Remember, this only works in jupyter!
-
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
-
-
-def player_input():
-    marker = ""
-    while not (marker == 'X' or marker == 'O'):
-        marker = input('Player 1: Do you want to be X or O? ').upper()
-    if marker == 'X':
-        return ('X', 'O')
-    else:
-        return ('O', 'X')
-
-
-def place_marker(board, marker, position):
-    board[position] = marker
-
-def win_check(board,mark):
-    
-    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
-    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
-    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
-    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
-    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
-    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
-    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
-    (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
-
+# randomly making a player start first
 import random
 
-def choose_first():
-    if random.randint(0, 1) == 0:
-        return 'Player 2'
-    else:
-        return 'Player 1'
 
-def space_check(board, position):
-    
-    return board[position] == ' '
+class Game:
+    # Creating the tic tac board
+    def display_board(self, board):
+        clear_output()  # For clearing output everytime
+        # Making board using print statement and board as list
+        self.board=board;
+        print('   |   |')
+        print(' ' + self.board[7] + ' | ' +
+              self.board[8] + ' | ' + self.board[9])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + self.board[4] + ' | ' +
+              self.board[5] + ' | ' + self.board[6])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + self.board[1] + ' | ' +
+              self.board[2] + ' | ' + self.board[3])
+        print('   |   |')
 
-def full_board_check(board):
-    for i in range(1,10):
-        if space_check(board, i):
-            return False
-    return True
+    # For taking input from the user
+    def player_input(self):
+        # setting marker as null
+        marker = ""
+        # checking if marker is X or O
+        while not (marker == 'X' or marker == 'O'):
+            marker = input('Player 1: Do you want to be X or O? ').upper()
+            # returning marker tuple
+        if marker == 'X':
+            return ('X', 'O')
+        else:
+            return ('O', 'X')
 
-def player_choice(board):
-    position = 0
-    
-    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
-        position = int(input('Choose your next position: (1-9) '))
-        
-    return position
+    # for placing the marker on board
+    def place_marker(self, board, marker, position):
+        self.board=board;
+        self.marker=marker;
+        self.position=position;
+        self.board[self.position] = self.marker
 
-def replay():
-    
-    return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+    # defining all the win conditions
+    def win_check(self, board, mark):
+        self.board=board;
+        self.mark=mark;
+        return ((self.board[7] == self.mark and self.board[8] == self.mark and self.board[9] == self.mark) or  # across the top
+                # across the middle
+                (self.board[4] == self.mark and self.board[5] == self.mark and self.board[6] == self.mark) or
+                # across the bottom
+                (self.board[1] == self.mark and self.board[2] == self.mark and self.board[3] == self.mark) or
+                # down the middle
+                (self.board[7] == self.mark and self.board[4] == self.mark and self.board[1] == self.mark) or
+                # down the middle
+                (self.board[8] == self.mark and self.board[5] == self.mark and self.board[2] == self.mark) or
+                # down the right side
+                (self.board[9] == self.mark and self.board[6] == self.mark and self.board[3] == self.mark) or
+                # diagonal
+                (self.board[7] == self.mark and self.board[5] == self.mark and self.board[3] == self.mark) or
+                (self.board[9] == self.mark and self.board[5] == self.mark and self.board[1] == self.mark))  # diagonal
+
+    def choose_first(self):
+        if random.randint(0, 1) == 0:
+            return 'Player 2'
+        else:
+            return 'Player 1'
+
+    # to check if the space is available at that position
+    def space_check(self, board, position):
+        self.board=board;
+        self.position=position;
+        return self.board[self.position] == ' '
+
+    # to check if board is full
+    def full_board_check(self, board):
+        self.board=board;
+        for i in range(1, 10):
+            if self.space_check(self.board, i):
+                return False
+        return True
+
+    # to get choice of postion from user
+    def player_choice(self, board):
+        position = 0
+        self.board=board;
+        while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not self.space_check(self.board, position):
+            position = int(input('Choose your next position: (1-9) '))
+
+        return position
+
+    # to play agaim
+    def replay(self):
+        return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+
 
 print('Welcome to Tic Tac Toe!')
+new_game = Game()
 
 while True:
-    # Reset the board
+    # To Reset the board
     theBoard = [' '] * 10
-    player1_marker, player2_marker = player_input()
-    turn = choose_first()
+    # To get marker
+    player1_marker, player2_marker = new_game.player_input()
+    # To get turn
+    turn = new_game.choose_first()
     print(turn + ' will go first.')
     
     play_game = input('Are you ready to play? Enter Yes or No.')
-    
+
     if play_game.lower()[0] == 'y':
         game_on = True
     else:
@@ -90,18 +120,18 @@ while True:
     while game_on:
         if turn == 'Player 1':
             # Player1's turn.
-            
-            display_board(theBoard)
-            position = player_choice(theBoard)
-            place_marker(theBoard, player1_marker, position)
 
-            if win_check(theBoard, player1_marker):
-                display_board(theBoard)
+            new_game.display_board(theBoard)
+            position = new_game.player_choice(theBoard)
+            new_game.place_marker(theBoard, player1_marker, position)
+
+            if new_game.win_check(theBoard, player1_marker):
+                new_game.display_board(theBoard)
                 print('Congratulations! You have won the game!')
                 game_on = False
             else:
-                if full_board_check(theBoard):
-                    display_board(theBoard)
+                if new_game.full_board_check(theBoard):
+                    new_game.display_board(theBoard)
                     print('The game is a draw!')
                     break
                 else:
@@ -109,24 +139,22 @@ while True:
 
         else:
             # Player2's turn.
-            
-            display_board(theBoard)
-            position = player_choice(theBoard)
-            place_marker(theBoard, player2_marker, position)
 
-            if win_check(theBoard, player2_marker):
-                display_board(theBoard)
+            new_game.display_board(theBoard)
+            position = new_game.player_choice(theBoard)
+            new_game.place_marker(theBoard, player2_marker, position)
+
+            if new_game.win_check(theBoard, player2_marker):
+                new_game.display_board(theBoard)
                 print('Player 2 has won!')
                 game_on = False
             else:
-                if full_board_check(theBoard):
-                    display_board(theBoard)
+                if new_game.full_board_check(theBoard):
+                    new_game.display_board(theBoard)
                     print('The game is a draw!')
                     break
                 else:
                     turn = 'Player 1'
 
-    if not replay():
+    if not new_game.replay():
         break
-
-
